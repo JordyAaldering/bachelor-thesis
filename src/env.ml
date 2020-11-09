d5053ccf62444b2ec334513796780a3a15a0884d
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-09 14:27:17 +0100

    Rewrite env

--- src/env.ml
+++ src/env.ml
@@ -3,15 +3,14 @@ exception EnvFailure of string
 type env = (string * string) list;;
 
 let env_new: unit -> env = fun () -> []
+
 let env_add e v p = (v, p) :: e
 
 let rec env_lookup e v = match e with
     | [] -> raise (EnvFailure (Printf.sprintf "lookup of variable `%s' failed" v) )
-    | (v', p') :: tl ->
-        if v' = v then
-            p'
-        else
-            env_lookup tl v
+    | (v', p')::tl ->
+        if v' = v then p'
+        else env_lookup tl v
 
 let env_to_str e =
     if List.length e = 0 then
