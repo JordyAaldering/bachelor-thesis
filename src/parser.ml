26bd84c25232e0320af48f5eebccb4916fd8af0e
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-12 13:29:40 +0100

    Parser rewrite and cleanup

--- src/parser.ml
+++ src/parser.ml
@@ -24,12 +24,7 @@ let op_prec tok = match tok with
 let tok_stack = ref []
 
 (* A shortcut for raising an exception *)
-let parse_err msg =
-    raise @@ ParseFailure (sprintf "Error: %s" msg)
-
-(* Puts the token back on the top of the stack *)
-let unget_tok tok =
-    tok_stack := tok::!tok_stack
+let parse_err msg = raise @@ ParseFailure (sprintf "Error: %s" msg)
 
 let get_token lexbuf = match !tok_stack with
     | [] -> token lexbuf
@@ -37,154 +32,148 @@ let get_token lexbuf = match !tok_stack with
         tok_stack := t;
         h
 
+(* Puts the token back on the top of the stack *)
+let unget_token tok =
+    tok_stack := tok::!tok_stack
+
 let peek_token lexbuf =
     let t = get_token lexbuf in
-    unget_tok t;
+    unget_token t;
     t
+
+let match_token lexbuf expected =
+    let t = peek_token lexbuf in
+    if t = expected then
+        let _step = get_token lexbuf in
+        true
+    else false
 
 let expect_id lexbuf =
     let t = get_token lexbuf in
     match t with
-        | ID x -> t
-        | _ -> parse_err
-            @@ sprintf "expected identifier, found `%s' instead"
+        | ID _x -> t
+        | _ -> parse_err @@ sprintf "expected identifier, found `%s' instead"
                 (token_to_str t)
 
-let expect_tok lexbuf t_exp =
+let expect_token lexbuf expected =
     let t = get_token lexbuf in
-    if t <> t_exp then
-        parse_err
-            @@ sprintf "expected token `%s', found `%s' instead"
-                (token_to_str t_exp)
-                (token_to_str t);
+    if t <> expected then
+        parse_err @@ sprintf "expected token `%s', found `%s' instead"
+            (token_to_str expected) (token_to_str t);
     t
-
-(* Parse non-empty comma separated list of elements that
-   can be parsed by `parse_fun' function *)
-let rec parse_generic_list ?(msg="expression expected") lexbuf parse_fun =
-    let e = parse_fun lexbuf in
-    if e = None then
-        parse_err msg;
-    let t = peek_token lexbuf in
-    if t = COMMA then
-        let _ = get_token lexbuf in
-        let l = parse_generic_list ~msg:msg lexbuf parse_fun in
-        e :: l
-    else [e]
 
 let rec parse_primary lexbuf =
     let t = get_token lexbuf in
     match t with
-        | ID x ->  Some (EVar x)
+        | ID x -> Some (EVar x)
         | INT x -> Some (EConst (float_of_int x))
         | FLOAT x -> Some (EConst x)
 
-        | IF -> unget_tok t; parse_ifthen lexbuf
-        | LET -> unget_tok t; parse_letin lexbuf
+        | IF -> parse_ifthen lexbuf
+        | LET -> parse_letin lexbuf
 
         | LSQUARE ->
-            let lst = if (peek_token lexbuf) = RSQUARE then
-                    []
-                else
-                    parse_generic_list lexbuf parse_expr
-                        ~msg:"array element definition is missing"
-            in let _ = expect_tok lexbuf RSQUARE in
+            let lst = if (peek_token lexbuf) <> RSQUARE then
+                    parse_array lexbuf parse_expr
+                else []
+            in let _square = expect_token lexbuf RSQUARE in
             Some (EArray (List.map opt_get lst))
 
         | LPAREN ->
             let e = parse_expr lexbuf in
             if e = None then
                 parse_err "empty expression found";
-            let _ = expect_tok lexbuf RPAREN in
+            let _paren = expect_token lexbuf RPAREN in
             e
 
-        | _ -> unget_tok t; None
+        | _ -> unget_token t; None
 
+(* Parse non-empty comma separated list of elements that
+   can be parsed by `parse_fun' function *)
+and parse_array lexbuf parse_fun =
+    let e = parse_fun lexbuf in
+    if e = None then
+        parse_err "expected array element definition";
+    if match_token lexbuf COMMA then
+        let l = parse_array lexbuf parse_fun in
+        e :: l
+    else [e]
 
 and parse_postfix lexbuf =
     let e = ref @@ parse_primary lexbuf in
-    while !e <> None && (peek_token lexbuf) = DOT do
-        let _ = get_token lexbuf in
+    while !e <> None && match_token lexbuf DOT do
         let e1 = parse_primary lexbuf in
         if e1 = None then
             parse_err "expected index specification in selection";
-        e := Some (ESel ((opt_get !e), (opt_get e1)))
+        e := Some (ESel (opt_get !e, opt_get e1))
     done;
     !e
 
-and parse_application ?(e1=None) lexbuf  =
+and parse_expr lexbuf =
+    parse_binary lexbuf
+
+and parse_application ?(e1=None) lexbuf =
     match e1, parse_unary lexbuf with
         | None, Some e2 -> parse_application lexbuf ~e1:(Some e2)
         | Some e1, Some e2 -> parse_application lexbuf ~e1:(Some (EApply (e1, e2)))
         | _, None -> e1
 
-and parse_unary lexbuf =
-    parse_postfix lexbuf
+and parse_letin lexbuf =
+    let id = expect_id lexbuf in
+    let _eq = expect_token lexbuf EQ in
+    let e1 = parse_expr lexbuf in
+    if e1 = None then
+        parse_err "expression expected after `='";
+    let _in = expect_token lexbuf IN in
+    let e2 = parse_expr lexbuf in
+    if e2 = None then
+        parse_err "expression expected after `in'";
+    Some (ELetIn (token_to_str id, opt_get e1, opt_get e2))
+
+and parse_ifthen lexbuf =
+    let ec = parse_expr lexbuf in
+    if ec = None then
+        parse_err "expression expected after `if'";
+    let _then = expect_token lexbuf THEN in
+    let et = parse_expr lexbuf in
+    if et = None then
+        parse_err "expression expected after `then'";
+    let _else = expect_token lexbuf ELSE in
+    let ef = parse_expr lexbuf in
+    if ef = None then
+        parse_err "expression expected after `else'";
+    Some (EIfThen (opt_get ec, opt_get et, opt_get ef))
 
 and parse_binary lexbuf =
     let rec resolve_stack s prec =
         let e1, op1, p1 = Stack.pop s in
         if prec <= p1 then (
             let e2, op2, p2 = Stack.pop s in
-            let e = EBinary ((op_to_binop op1), (opt_get e2), (opt_get e1)) in
+            let e = EBinary (op_to_binop op1, opt_get e2, opt_get e1) in
             Stack.push (Some e, op2, p2) s;
             resolve_stack s prec
-        ) else (
-            Stack.push (e1, op1, p1) s
-        )
+        ) else Stack.push (e1, op1, p1) s
     in
     let e1 = parse_application lexbuf in
-    if e1 = None then e1
-    else
+    if e1 <> None then
         let s = Stack.create () in
-        (* First expression goes with no priority, priority = -1 *)
+        (* first expression goes with no priority; -1 *)
         Stack.push (e1, EOF, -1) s;
-        while is_op (peek_token lexbuf) do
+        while is_op @@ peek_token lexbuf do
             let t = get_token lexbuf in
-
-            (* resolve priority stack *)
-            resolve_stack s (op_prec t);
-            let e2 = parse_application lexbuf in
-            if e2 = None then
-                parse_err @@ sprintf "expression expected after %s" (token_to_str t);
-            Stack.push (e2, t, (op_prec t)) s;
+                resolve_stack s (op_prec t);
+                let e2 = parse_application lexbuf in
+                if e2 = None then
+                    parse_err @@ sprintf "expression expected after %s" (token_to_str t);
+                Stack.push (e2, t, op_prec t) s;
         done;
         resolve_stack s 0;
-        let e, op, prec = Stack.pop s in
+        let e, _op, _prec = Stack.pop s in
         e
+    else e1
 
-and parse_expr lexbuf =
-    parse_binary lexbuf
-
-and parse_letin lexbuf =
-    let t = get_token lexbuf in
-    assert (LET = t);
-    let t = expect_id lexbuf in
-    let _ = expect_tok lexbuf EQ in
-    let e1 = parse_expr lexbuf in
-    if e1 = None then
-        parse_err "expression expected after `='";
-    let _ = expect_tok lexbuf IN in
-    let e2 = parse_expr lexbuf in
-    if e2 = None then
-        parse_err "expression expected after `in'";
-    Some (ELetIn ((token_to_str t), (opt_get e1), (opt_get e2)))
-
-and parse_ifthen lexbuf =
-    let t = get_token lexbuf in
-    assert (IF = t);
-    let e1 = parse_expr lexbuf in
-    if e1 = None then
-        parse_err "expression expected after `if'";
-    let _ = expect_tok lexbuf THEN in
-    let e2 = parse_expr lexbuf in
-    if e2 = None then
-        parse_err "expression expected after `then'";
-    let _ = expect_tok lexbuf ELSE in
-    let e3 = parse_expr lexbuf in
-    if e3 = None then
-        parse_err "expression expected after `else'";
-    Some (EIfThen ((opt_get e1), (opt_get e2), (opt_get e3)))
+and parse_unary lexbuf =
+    parse_postfix lexbuf
 
 let prog lexbuf =
     tok_stack := [];
