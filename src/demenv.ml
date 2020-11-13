7ff60e1916b806dc8d021f5e3265d81dd08c5ac7
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-13 17:01:11 +0100

    sd fixes

--- src/demenv.ml
+++ src/demenv.ml
@@ -23,32 +23,33 @@ let dem_env_set: dem_env -> string -> int array -> dem_env = fun st x dem ->
         let dem_old = Hashtbl.find st x in
         let dem_oplus = Array.map2 max dem dem_old in
         Hashtbl.replace st x dem_oplus;
-        st
+        Hashtbl.copy st
     with Not_found ->
         Hashtbl.add st x dem;
-        st
+        Hashtbl.copy st
 
 let dem_env_combine: dem_env -> dem_env -> dem_env = fun xs ys ->
+    let union = Hashtbl.copy xs in 
     Hashtbl.iter (fun y dem_y ->
         try
-            let dem_x = Hashtbl.find xs y in
+            let dem_x = Hashtbl.find union y in
             let dem_max = Array.map2 max dem_x dem_y in
-            Hashtbl.replace xs y dem_max;
+            Hashtbl.replace union y dem_max;
         with Not_found ->
-            Hashtbl.add xs y dem_y;
+            Hashtbl.add union y dem_y;
     ) ys;
-    xs
+    union
 
 let dem_env_remove st p =
     Hashtbl.remove st p;
-    st
+    Hashtbl.copy st
 
 let dem_env_lookup st p =
     dem_env_find_and_raise st p true
         @@ sprintf "Attempt to lookup non-existing pointer `%s'" p;
     Hashtbl.find st p
 
-let dem_env_to_str st =
+let dem_env_to_str: dem_env -> string = fun st ->
     Hashtbl.fold (fun k v tail ->
-        sprintf "%s -> %s\n%s" k (String.concat ", " (List.map string_of_int v)) tail
+        sprintf "%s -> %s\n%s" k (String.concat ", " (Array.to_list (Array.map string_of_int v))) tail
     ) st ""
--- src/sd.ml
+++ src/sd.ml
@@ -14,46 +14,75 @@ let pv_get: (int array) list -> int -> int array -> int array = fun pv i dem_ivs
         Array.get dem i
     ) dem_ivs
 
-let rec sd: expr -> int array -> dem_env = fun e dem -> match e with
+let rec sd: expr -> int array -> dem_env -> dem_env = fun e dem f -> match e with
     | EVar x -> dem_env_new x dem
     | EConst _x -> dem_env_empty ()
     | EArray _xs -> dem_env_empty ()
 
-    | EApply (e1, e2) -> (match e1 with
-        | ELambda (x, e)
-        | _ -> rewrite_err "Invalid SD Apply argument"
+    | EApply (e1, arg) -> (match e1 with
+        | EVar x ->
+            let fun_pv = dem_env_lookup f x in
+            let dem_pv = pv_get [fun_pv] 0 dem in
+            sd arg dem_pv f
+        | ELambda (x, el) ->
+            let fun_pv = pv e1 f in
+            let dem_pv = pv_get fun_pv 0 dem in
+            sd arg dem_pv f
+        | _ -> rewrite_err @@ sprintf "Invalid SD Apply argument `%s'" (expr_to_str e)
     )
-
-    | ELetIn (x, e1, e2) ->
-        let pv = pv @@ ELambda (x, e2) in
-        let env_e1 = sd e1 (pv_get pv 0 dem) in
-        let env_e2 = sd e2 dem in
-        let env_e2 = dem_env_remove env_e2 x in
-        dem_env_combine env_e1 env_e2
+    | ELambda (x, e1) ->
+        let fun_pv = pv e f in
+        let f = dem_env_set f x (List.hd fun_pv) in
+        let dem_pv = pv_get fun_pv 0 dem in
+        sd e1 dem_pv f
+    | ELetIn (x, e1, e2) -> (match e1 with
+        | ELambda (var, lambda) -> (
+            let lambda_pv = pv e1 f in
+            let f = dem_env_set f x (List.hd lambda_pv) in
+            let env_let = sd e1 (pv_get lambda_pv 0 dem) f in
+            let env_in = sd e2 dem f in
+            let env_in = dem_env_remove env_in var in
+            dem_env_combine env_let env_in
+        )
+        | _ -> (
+            let dem_pv = pv (ELambda (x, e2)) f in
+            let env_e1 = sd e1 (pv_get dem_pv 0 dem) f in
+            let env_e2 = sd e2 dem f in
+            let env_e2 = dem_env_remove env_e2 x in
+            dem_env_combine env_e1 env_e2
+        )
+    )
     | EIfThen (ec, et, ef) ->
-        let env_ec = sd ec [|0;3;3;3|] in
-        let env_et = sd et dem in
-        let env_ef = sd ef dem in
+        let env_ec = sd ec [|0;3;3;3|] f in
+        let env_et = sd et dem f in
+        let env_ef = sd ef dem f in
         dem_env_combine (dem_env_combine env_ec env_et) env_ef
 
+    | EBinary (op, el, er) ->
+        let dem_pv = pv e f in
+        let env_el = sd el (pv_get dem_pv 0 dem) f in
+        let env_er = sd er (pv_get dem_pv 1 dem) f in
+        dem_env_combine env_el env_er
+    | EUnary (op, er) ->
+        let dem_pv = pv e f in
+        sd er (pv_get dem_pv 0 dem) f
+
     | ESel (iv, v) ->
-        let dem_pv = pv e1 in
-        let env_iv = sd iv (pv_get dem_pv 0 dem) in
-        let env_v = sd v (pv_get dem_pv 1 dem) in
+        let dem_pv = pv e f in
+        let env_iv = sd iv (pv_get dem_pv 0 dem) f in
+        let env_v = sd v (pv_get dem_pv 1 dem) f in
         dem_env_combine env_iv env_v
     | EShape x
     | EDim x ->
-        let dem_0 = pv_get (pv e1) 0 dem in
-        sd x dem_0
+        let dem_pv = pv e f in
+        sd x (pv_get dem_pv 0 dem) f
 
-    | _ -> rewrite_err "Invalid SD argument"
-
-and pv: expr -> (int array) list = fun e -> match e with
+and pv: expr -> dem_env -> (int array) list = fun e f -> match e with
     | ELambda (x, e) ->
-        let env = sd e [|0;1;2;3|] in
+        let env = sd e [|0;1;2;3|] f in
         let dem = dem_env_lookup env x in
         [ dem ]
-    | EBinary (op, _e1, _e2) -> (match op with
+    | EBinary (op, _l, _r) -> (match op with
         | OpPlus
         | OpMin
         | OpMult
@@ -65,7 +94,7 @@ and pv: expr -> (int array) list = fun e -> match e with
         | OpGt
         | OpGe -> [ [|0;0;0;3|]; [|0;0;0;3|] ]
     )
-    | EUnary (op, _e) -> (match op with
+    | EUnary (op, _r) -> (match op with
         | OpNeg -> [ [|0;1;2;3|] ]
         | OpNot -> [ [|0;0;0;3|] ]
     )
@@ -73,4 +102,7 @@ and pv: expr -> (int array) list = fun e -> match e with
     | EShape _ -> [ [|0;0;1;2|] ]
     | EDim _   -> [ [|0;0;0;1|] ]
 
-    | _ -> rewrite_err "Invalid PV argument"
+    | _ -> rewrite_err @@ sprintf "Invalid PV argument `%s'" (expr_to_str e)
+
+let sd_prog: expr -> dem_env = fun e ->
+    sd e [|0;1;2;3|] (dem_env_empty ())
