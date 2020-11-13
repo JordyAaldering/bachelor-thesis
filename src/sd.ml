64984dc47ed4a1abdcfa2d060afd78d859a384c6
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-13 15:01:49 +0100

    SD if then case

--- src/sd.ml
+++ src/sd.ml
@@ -3,6 +3,11 @@ open Pv
 open Demenv
 open Printf
 
+exception RewriteFailure of string
+
+let rewrite_err msg = raise @@ RewriteFailure msg
+
+
 let pv_get: (int array) list -> int -> int array -> int array = fun pv i dem_ivs ->
     let dem = List.nth pv i in
     Array.map (fun i ->
@@ -15,20 +20,19 @@ let rec sd: expr -> int array -> dem_env = fun e dem -> match e with
     | EArray _xs -> dem_env_empty ()
 
     | EApply (e1, e2) -> dem_env_empty ()
-    | ELambda (x, e) -> dem_env_empty ()
     | ELetIn (x, e1, e2) ->
         let pv = pv @@ ELambda (x, e2) in
         let env_e1 = sd e1 (pv_get pv 0 dem) in
         let env_e2 = sd e2 dem in
         let env_e2 = dem_env_remove env_e2 x in
         dem_env_combine env_e1 env_e2
-    | EIfThen (e1, e2, e3) -> dem_env_empty ()
+    | EIfThen (ec, et, ef) ->
+        let env_ec = sd ec [|0;3;3;3|] in
+        let env_et = sd et dem in
+        let env_ef = sd ef dem in
+        dem_env_combine (dem_env_combine env_ec env_et) env_ef
 
-    | EBinary (op, e1, e2) -> dem_env_empty ()
-    | EUnary (op, e) -> dem_env_empty ()
-    | ESel (e1, e2) -> dem_env_empty ()
-    | EShape e -> dem_env_empty ()
-    | EDim e -> dem_env_empty ()
+    | _ -> rewrite_err "Invalid SD argument"
 
 and pv: expr -> (int array) list = fun e -> match e with
     | ELambda (x, e) ->
@@ -55,4 +59,4 @@ and pv: expr -> (int array) list = fun e -> match e with
     | EShape _ -> [ [|0;0;1;2|] ]
     | EDim _ -> [ [|0;0;0;1|] ]
 
-    | _ -> [ [|0;1;2;3|] ]
+    | _ -> rewrite_err "Invalid PV argument"
