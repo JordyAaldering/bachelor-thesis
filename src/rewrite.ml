37f81b064dc1f9547601b1eb3278516859f66679
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-15 14:32:49 +0100

    Rewrite lambda case

--- src/rewrite.ml
+++ src/rewrite.ml
@@ -3,17 +3,31 @@ open Ast
 open Printf
 
 module Eval_env = Map.Make(String)
+type eval_env = int Eval_env.t
+
 
 exception RewriteFailure of string
-
 let rewrite_err msg = raise @@ RewriteFailure msg
-
 let rewrite_err_in e msg = raise @@ RewriteFailure (sprintf "%s: %s" (expr_to_str e) msg)
 
-let rec rewrite_f: expr -> int Eval_env.t -> expr = fun e env -> match e with
+
+let rec rewrite: expr -> int -> int Eval_env.t -> expr = fun e lvl env -> match lvl with
+    | 3 -> rewrite_f e env
+    | 2 -> rewrite_s e env
+    | 1 -> rewrite_d e env
+    | 0 -> EConst 0.
+    | _ -> rewrite_err_in e @@ sprintf "invalid eval level `%d'" lvl
+
+and rewrite_f: expr -> int Eval_env.t -> expr = fun e env -> match e with
     | EVar x -> e
     | EConst x -> e
     | EArray x -> e
+
+    | ELambda (x, e1) ->
+        let dem = pv e Dem_env.empty in
+        let lvl = Array.get (List.hd dem) 3 in
+        let env' = Eval_env.add x lvl env in
+        ELambda (x, rewrite_f e1 env')
 
     | _ -> e
 
@@ -30,9 +44,14 @@ and rewrite_s: expr -> int Eval_env.t -> expr = fun e env -> match e with
         with Not_found ->
             rewrite_err_in e @@ sprintf "key `%s' was not found" x
     end
-
     | EConst x -> EShape e
     | EArray x -> EShape e
+
+    | ELambda (x, e1) ->
+        let dem = pv e Dem_env.empty in
+        let lvl = Array.get (List.hd dem) 2 in
+        let env' = Eval_env.add x lvl env in
+        ELambda (x, rewrite_s e1 env')
 
     | _ -> e
 
@@ -51,9 +70,14 @@ and rewrite_d: expr -> int Eval_env.t -> expr = fun e env -> match e with
         with Not_found ->
             rewrite_err_in e @@ sprintf "key `%s' was not found" x
     end
-
     | EConst x -> EDim e
     | EArray x -> EDim e
+
+    | ELambda (x, e1) ->
+        let dem = pv e Dem_env.empty in
+        let lvl = Array.get (List.hd dem) 1 in
+        let env' = Eval_env.add x lvl env in
+        ELambda (x, rewrite_d e1 env')
 
     | _ -> e
 
--- src/sd.ml
+++ src/sd.ml
@@ -3,89 +3,29 @@ open Pv
 open Demenv
 open Printf
 
-exception SdFailure of string
+module Dem_env = Map.Make(String)
+type dem_env = ((int array) list) Dem_env.t
 
+
+exception SdFailure of string
 let sd_err msg = raise @@ SdFailure msg
 
 
-let pv_get: (int array) list -> int -> int array -> int array = fun pv i dem_ivs ->
+let pv_get: (int array) list -> int -> int array -> int array = fun pv i iv ->
     let dem = List.nth pv i in
-    Array.map (fun i ->
-        Array.get dem i
-    ) dem_ivs
+    Array.map (Array.get dem) iv
 
-let rec sd: expr -> int array -> dem_env -> dem_env = fun e dem f -> match e with
-    | EVar x -> dem_env_set f x dem; f
-    | EConst _x -> f
-    | EArray _xs -> f
-
-    | EApply (e1, e2) -> (match e1 with
-        | EVar x -> (* e2 is applied to a var, e.g. the name of a lambda function *)
-            let x_dem = dem_env_lookup f x in
-            let dem' = pv_get [x_dem] 0 dem in
-            sd e2 dem' f
-        | ELambda (x, el) -> (* e1 is a local lambda, which e2 is applied to *)
-            let lambda_pv = pv e1 f in
-            dem_env_set f x (List.hd lambda_pv);
-            let dem' = pv_get lambda_pv 0 dem in
-            let env_lambda = sd el dem' f in
-            let f' = sd e2 dem' env_lambda in
-            dem_env_remove f' x;
-            f'
-        | _ -> sd_err @@ sprintf "Invalid SD Apply arguments `%s' and `%s'"
-                (expr_to_str e1) (expr_to_str e2)
-    )
-    | ELetIn (x, e1, e2) -> (match e1 with
-        | ELambda (var, lambda) -> (
-            let lambda_pv = pv e1 f in
-            dem_env_set f x (List.hd lambda_pv);
-            let env_e1 = sd lambda (pv_get lambda_pv 0 dem) f in
-            let env_e2 = sd e2 dem f in
-            dem_env_remove env_e1 var;
-            let f' = dem_env_combine env_e1 env_e2 in
-            dem_env_remove f' var;
-            f'
-        )
-        | _ -> (
-            let dem_pv = pv (ELambda (x, e2)) f in
-            let env_e1 = sd e1 (pv_get dem_pv 0 dem) f in
-            let env_e2 = sd e2 dem f in
-            dem_env_remove env_e2 x;
-            dem_env_combine env_e1 env_e2
-        )
-    )
-    | EIfThen (ec, et, ef) ->
-        let env_ec = sd ec [|0;3;3;3|] f in
-        let env_et = sd et dem f in
-        let env_ef = sd ef dem f in
-        dem_env_combine (dem_env_combine env_ec env_et) env_ef
-
-    | EBinary (op, el, er) ->
-        let dem_pv = pv e f in
-        let env_el = sd el (pv_get dem_pv 0 dem) f in
-        let env_er = sd er (pv_get dem_pv 1 dem) f in
-        dem_env_combine env_el env_er
-    | EUnary (op, er) ->
-        let dem_pv = pv e f in
-        sd er (pv_get dem_pv 0 dem) f
-
-    | ESel (iv, v) ->
-        let dem_pv = pv e f in
-        let env_iv = sd iv (pv_get dem_pv 0 dem) f in
-        let env_v = sd v (pv_get dem_pv 1 dem) f in
-        dem_env_combine env_iv env_v
-    | EShape x
-    | EDim x ->
-        let dem_pv = pv e f in
-        sd x (pv_get dem_pv 0 dem) f
+let rec sd: expr -> int array -> dem_env -> dem_env = fun e dem env -> match e with
+    | EVar x -> Dem_env.add x [dem] env
+    | EConst _x -> env
+    | EArray _xs -> env
 
     | _ -> sd_err @@ sprintf "Invalid SD argument `%s'" (expr_to_str e)
 
-and pv: expr -> dem_env -> (int array) list = fun e f -> match e with
+and pv: expr -> dem_env -> (int array) list = fun e env -> match e with
     | ELambda (x, e) ->
-        let env = sd e [|0;1;2;3|] f in
-        let dem = dem_env_lookup env x in
-        [ dem ]
+        let env' = sd e [|0;1;2;3|] env in
+        Dem_env.find x env'
     | EBinary (op, _l, _r) -> (match op with
         | OpPlus
         | OpMin
@@ -109,4 +49,4 @@ and pv: expr -> dem_env -> (int array) list = fun e f -> match e with
     | _ -> sd_err @@ sprintf "Invalid PV argument `%s'" (expr_to_str e)
 
 let sd_prog: expr -> dem_env = fun e ->
-    sd e [|0;1;2;3|] (mk_dem_env ())
+    sd e [|0;1;2;3|] Dem_env.empty
