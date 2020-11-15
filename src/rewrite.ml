7121cc91869a1a8c59ffab842006b79cc1ccc066
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-15 14:45:30 +0100

    Rewrite if case

--- src/rewrite.ml
+++ src/rewrite.ml
@@ -28,14 +28,8 @@ and rewrite_f: expr -> int Eval_env.t -> expr = fun e env -> match e with
         let lvl = Array.get (List.hd dem) 3 in
         let env' = Eval_env.add x lvl env in
         ELambda (x, rewrite_f e1 env')
-    | ELetIn (x, e1, e2) ->
-        let dem = pv (ELambda (x, e2)) Dem_env.empty in
-        let lvl = Array.get (List.hd dem) 3 in
-        if lvl = 0 then
-            rewrite_f e2 env
-        else
-            let env' = Eval_env.add x lvl env in
-            ELetIn (x, rewrite e1 lvl env, rewrite_f e2 env')
+    | ELetIn (x, e1, e2) -> rewrite_let e 3 env
+    | EIfThen _ -> rewrite_if e 3 env
 
     | _ -> e
 
@@ -60,14 +54,8 @@ and rewrite_s: expr -> int Eval_env.t -> expr = fun e env -> match e with
         let lvl = Array.get (List.hd dem) 2 in
         let env' = Eval_env.add x lvl env in
         ELambda (x, rewrite_s e1 env')
-    | ELetIn (x, e1, e2) ->
-        let dem = pv (ELambda (x, e2)) Dem_env.empty in
-        let lvl = Array.get (List.hd dem) 2 in
-        if lvl = 0 then
-            rewrite_s e2 env
-        else
-            let env' = Eval_env.add x lvl env in
-            ELetIn (x, rewrite e1 lvl env, rewrite_s e2 env')
+    | ELetIn (x, e1, e2) -> rewrite_let e 2 env
+    | EIfThen _ -> rewrite_if e 2 env
 
     | _ -> e
 
@@ -94,16 +82,25 @@ and rewrite_d: expr -> int Eval_env.t -> expr = fun e env -> match e with
         let lvl = Array.get (List.hd dem) 1 in
         let env' = Eval_env.add x lvl env in
         ELambda (x, rewrite_d e1 env')
-    | ELetIn (x, e1, e2) ->
-        let dem = pv (ELambda (x, e2)) Dem_env.empty in
-        let lvl = Array.get (List.hd dem) 1 in
-        if lvl = 0 then
-            rewrite_d e2 env
-        else
-            let env' = Eval_env.add x lvl env in
-            ELetIn (x, rewrite e1 lvl env, rewrite_d e2 env')
+    | ELetIn (x, e1, e2) -> rewrite_let e 1 env
+    | EIfThen _ -> rewrite_if e 1 env
 
     | _ -> e
+
+and rewrite_let: expr -> int -> int Eval_env.t -> expr = fun e lvl env -> match e with
+    | ELetIn (x, e1, e2) ->
+        let dem = pv (ELambda (x, e2)) Dem_env.empty in
+        let lvl' = Array.get (List.hd dem) lvl in
+        if lvl' = 0 then
+            rewrite e2 lvl env
+        else
+            let env' = Eval_env.add x lvl' env in
+            ELetIn (x, rewrite e1 lvl' env, rewrite e2 lvl env')
+    | _ -> assert false
+
+and rewrite_if: expr -> int -> int Eval_env.t -> expr = fun e lvl env -> match e with
+    | EIfThen (ec, et, ef) -> EIfThen (rewrite_f ec env, rewrite et lvl env, rewrite ef lvl env)
+    | _ -> assert false
 
 let rewrite_prog: expr -> expr = fun e ->
     rewrite_f e Eval_env.empty
