077be29d1db6d2b51c5657996a30371cc5d70cf3
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-21 16:22:09 +0100

    Cleanup

--- lib/parser.mli
+++ lib/parser.mli
@@ -2,9 +2,9 @@ open Lexing
 
 val parse_err : string -> unit
 
-val parse_postfix : lexbuf -> Ast.expr
-
 val parse_expr : lexbuf -> Ast.expr
+
+val parse_postfix : lexbuf -> Ast.expr
 
 val parse_application : Ast.expr -> lexbuf -> Ast.expr
 
@@ -14,12 +14,12 @@ val parse_letin : lexbuf -> Ast.expr option
 
 val parse_ifthen : lexbuf -> Ast.expr option
 
-val parse_shape : lexbuf -> Ast.expr option
-
-val parse_dim : lexbuf -> Ast.expr option
-
 val parse_binary : lexbuf -> Ast.expr option
 
 val parse_unary : lexbuf -> Ast.expr option
+
+val parse_shape : lexbuf -> Ast.expr option
+
+val parse_dim : lexbuf -> Ast.expr option
 
 val parse_prog : lexbuf -> Ast.expr
--- lib/value.mli
+++ lib/value.mli
@@ -5,19 +5,29 @@ val value_err : string -> unit
 val value_to_str : value -> string
 
 val value_sel : value -> value -> value
+
 val value_shape : value -> value
+
 val value_dim : value -> value
 
 val value_neg : value -> value
+
 val value_add : value -> value -> value
+
 val value_mul : value -> value -> value
+
 val value_div : value -> value -> value
 
 val value_is_truthy : value -> bool
+
 val value_not : value -> value
+
 val value_eq : value -> value -> value
+
 val value_gt : value -> value -> value
+
 val value_lt : value -> value -> value
 
 val value_to_pair : value -> (int list * float list)
+
 val closure_to_triple : value -> (string * Ast.expr * Env.ptr_env)
