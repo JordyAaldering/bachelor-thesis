3a93506123be25a0210a4ed40369aca44bc27497
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-31 13:34:09 +0100

    Remove lib

--- lib/ast.mli
+++
@@ -1,9 +0,0 @@
-type expr
-
-type bop
-
-type uop
-
-val parse_err : string -> unit
-
-val expr_to_str : string -> expr -> expr
--- lib/env.mli
+++
@@ -1,4 +0,0 @@
-(* An environment that points values to their definition *)
-type ptr_env
-
-val ptr_env_to_str : ptr_env -> string
