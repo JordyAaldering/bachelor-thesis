9889200aa64e1787a09d863b76a9254c2ff1089e
Jordy Aaldering <aalderingjordy@gmail.com>
2021-01-10 15:23:45 +0100

    Explicit typing

--- src/parser.ml
+++ src/parser.ml
@@ -1,58 +1,61 @@
 open Ast
 open Token
 open Lexer
+open Lexing
 open Printf
 
-let opt_get x = match x with
+let opt_get (x: expr option) : expr =
+    match x with
     | Some x -> x
     | None -> parse_err "expression was None"
 
 (* stack to keep tokens that we have peeked but not yet consumed *)
-let token_stack = ref []
+let token_stack : token list ref = ref []
 
-let get_token lexbuf = match !token_stack with
+let get_token (lexbuf: lexbuf) : token =
+    match !token_stack with
     | [] -> token lexbuf
     | h :: t ->
         token_stack := t;
         h
 
-let unget_token token =
+let unget_token (token: token) =
     token_stack := token :: !token_stack
 
-let peek_token lexbuf =
+let peek_token (lexbuf: lexbuf) : token =
     let t = get_token lexbuf in
     unget_token t;
     t
 
-let match_token lexbuf expected =
+let match_token (lexbuf: lexbuf) (expected: token) : bool =
     if peek_token lexbuf = expected then
         let _ = get_token lexbuf in
         true
     else false
 
-let expect_id lexbuf =
+let expect_id (lexbuf: lexbuf) : string =
     let t = get_token lexbuf in
     match t with
         | ID s -> s
         | _ -> parse_err @@ sprintf "expected identifier, found `%s'"
                 (token_to_str t)
 
-let expect_token lexbuf expected =
+let expect_token (lexbuf: lexbuf) (expected: token) =
     let t = get_token lexbuf in
     if t <> expected then
         parse_err @@ sprintf "expected token `%s', found `%s'"
             (token_to_str expected) (token_to_str t)
 
-let rec expect_expr lexbuf =
+let rec expect_expr (lexbuf: lexbuf) : expr =
     let e = parse_expr lexbuf in
     if e = None then
-        parse_err "expected expression";
+        parse_err "expected expression, found None";
     opt_get e
 
-and parse_expr lexbuf =
+and parse_expr (lexbuf: lexbuf) : expr option =
     parse_binary lexbuf
 
-and parse_primary lexbuf =
+and parse_primary (lexbuf: lexbuf) : expr option =
     let t = get_token lexbuf in
     match t with
         (* variables *)
@@ -80,9 +83,8 @@ and parse_primary lexbuf =
             e
         | _ -> unget_token t; None
 
-(* Parse non-empty comma separated list of elements
-    that can be parsed by `parse_fun' *)
-and parse_array lexbuf parse_fun =
+(* Parse non-empty comma separated list of elements that can be parsed by `parse_fun' *)
+and parse_array (lexbuf: lexbuf) (parse_fun: lexbuf -> expr option) : expr option list =
     let e = parse_fun lexbuf in
     if e = None then
         parse_err "expected array element definition";
@@ -91,7 +93,7 @@ and parse_array lexbuf parse_fun =
         e :: l
     else [e]
 
-and parse_postfix lexbuf =
+and parse_postfix (lexbuf: lexbuf) : expr option =
     let e = ref @@ parse_primary lexbuf in
     while !e <> None && match_token lexbuf DOT do
         let e1 = parse_primary lexbuf in
@@ -101,19 +103,19 @@ and parse_postfix lexbuf =
     done;
     !e
 
-and parse_application ?(e1=None) lexbuf =
+and parse_application ?(e1: expr option = None) (lexbuf: lexbuf) : expr option =
     match e1, parse_unary lexbuf with
         | None, Some e2 -> parse_application lexbuf ~e1:(Some e2)
         | Some e1, Some e2 -> parse_application lexbuf ~e1:(Some (EApply (e1, e2)))
         | _ -> e1
 
-and parse_lambda lexbuf =
+and parse_lambda (lexbuf: lexbuf) : expr option =
     let s = expect_id lexbuf in
     expect_token lexbuf DOT;
     let e1 = expect_expr lexbuf in
     Some (ELambda (s, e1))
 
-and parse_let lexbuf =
+and parse_let (lexbuf: lexbuf) : expr option =
     let s = expect_id lexbuf in
     expect_token lexbuf EQ;
     let e1 = expect_expr lexbuf in
@@ -121,7 +123,7 @@ and parse_let lexbuf =
     let e2 = expect_expr lexbuf in
     Some (ELet (s, e1, e2))
 
-and parse_cond lexbuf =
+and parse_cond (lexbuf: lexbuf) : expr option =
     let e1 = expect_expr lexbuf in
     expect_token lexbuf THEN;
     let e2 = expect_expr lexbuf in
@@ -129,7 +131,7 @@ and parse_cond lexbuf =
     let e3 = expect_expr lexbuf in
     Some (ECond (e1, e2, e3))
 
-and parse_binary lexbuf =
+and parse_binary (lexbuf: lexbuf) : expr option =
     let rec resolve_stack s prec =
         let e1, op1, p1 = Stack.pop s in
         if prec <= p1 then (
@@ -157,10 +159,10 @@ and parse_binary lexbuf =
         let e, _op, _prec = Stack.pop s in
         e
 
-and parse_unary lexbuf =
+and parse_unary (lexbuf: lexbuf) : expr option =
     parse_postfix lexbuf
 
-let parse path =
+let parse (path: string) : expr =
     token_stack := [];
     let file = open_in path in
     let lexbuf = Lexing.from_channel file in
