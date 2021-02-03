f806298784d4f407bb0e3cf2d2761fac92b9d593
Jordy Aaldering <aalderingjordy@gmail.com>
2021-02-03 15:07:57 +0100

    Ast cleanup

--- src/ast.ml
+++ src/ast.ml
@@ -76,31 +76,42 @@ let rec expr_to_str (e: expr) : string =
     (* variables *)
     | EVar s -> s
     | EFloat x -> sprintf "%g" x
-    | EArray xs -> sprintf "[%s]"
-        (String.concat ", " (List.map expr_to_str xs))
+    | EArray xs ->
+        sprintf "[%s]"
+            (String.concat ", " (List.map expr_to_str xs))
     (* expressions *)
-    | EApply (e1, e2) -> sprintf "%s %s"
-        (decide_paren e1) (decide_paren e2)
-    | ELambda (s, e) -> sprintf "\\%s. %s"
-        s (expr_to_str e)
-    | ELet (s, e1, e2) -> sprintf "let %s = %s in\n%s"
-        s (expr_to_str e1) (expr_to_str e2)
-    | ECond (e1, e2, e3) -> sprintf "if %s then %s else %s"
-        (expr_to_str e1) (expr_to_str e2) (expr_to_str e3)
-    | EWith (e1, e2, s, e3, e4) -> sprintf "gen %s with %s <= %s < %s do %s"
-        (expr_to_str e1) (expr_to_str e2) s (expr_to_str e3) (expr_to_str e4)
+    | EApply (e1, e2) ->
+        sprintf "%s %s"
+            (decide_paren e1) (decide_paren e2)
+    | ELambda (s, e) ->
+        sprintf "\\%s. %s"
+            s (expr_to_str e)
+    | ELet (s, e1, e2) ->
+        sprintf "let %s = %s in\n%s"
+            s (expr_to_str e1) (expr_to_str e2)
+    | ECond (e1, e2, e3) ->
+        sprintf "if %s then %s else %s"
+            (expr_to_str e1) (expr_to_str e2) (expr_to_str e3)
+    | EWith (e1, e2, s, e3, e4) ->
+        sprintf "gen %s with %s <= %s < %s do %s"
+            (expr_to_str e1) (expr_to_str e2) s (expr_to_str e3) (expr_to_str e4)
     (* operands *)
-    | EBinary (op, e1, e2) -> sprintf "%s %s %s"
-        (decide_paren e1) (bop_to_str op) (decide_paren e2)
-    | EUnary (op, e1) -> sprintf "%s%s"
-        (uop_to_str op) (decide_paren e1)
+    | EBinary (op, e1, e2) ->
+        sprintf "%s %s %s"
+            (decide_paren e1) (bop_to_str op) (decide_paren e2)
+    | EUnary (op, e1) ->
+        sprintf "%s%s"
+            (uop_to_str op) (decide_paren e1)
     (* primitive functions *)
-    | ESel (e1, e2) -> sprintf "%s.(%s)"
-        (decide_paren e1) (expr_to_str e2)
-    | EShape e1 -> sprintf "value_shape %s"
-        (decide_paren e1)
-    | EDim e1 -> sprintf "value_dim %s"
-        (decide_paren e1)
+    | ESel (e1, e2) ->
+        sprintf "%s.(%s)"
+            (decide_paren e1) (expr_to_str e2)
+    | EShape e1 ->
+        sprintf "shape %s"
+            (decide_paren e1)
+    | EDim e1 ->
+        sprintf "dim %s"
+            (decide_paren e1)
     | ERead -> "read"
 
 and decide_paren (e: expr) : string =
